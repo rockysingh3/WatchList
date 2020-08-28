711178c8498c4aab3b731c878ba9dcdca694a9fd
Rocky Singh <rockygsingh@icloud.com>
2020-08-28 12:43:51 -0400

    card to display all the movies in

---
+++ src/components/MovieCard.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const MovieCard = ({ movie, type }) => {
+    return (
+        <div className="movie-card">
+            <div className="overlay">
+
+            </div>
+        </div>
+    )
+}
