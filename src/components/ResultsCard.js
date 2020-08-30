00f010d0f30e9bbf3221af4dd1932990eb061ad3
Rocky Singh <rockygsingh@icloud.com>
2020-08-30 15:32:32 -0400

    removed import for moment

--- src/components/ResultsCard.js
+++ src/components/ResultsCard.js
@@ -1,16 +1,28 @@
 import React, { useContext } from "react";
+// import Moment from "react-moment";
 import { GlobalContext } from "../context/GlobalState";
 
 export const ResultsCard = ({ movie }) => {
-  const { addMovietoWatchlist, watchlist } = useContext(GlobalContext);
+  const {
+    addMovieToWatchlist,
+    addMovieToWatched,
+    watchlist,
+    watched,
+  } = useContext(GlobalContext);
 
-  const storeMovie = watchlist.find(o => o.id === movie.id)
+  let storedMovie = watchlist.find((o) => o.id === movie.id);
+  let storedMovieWatched = watched.find((o) => o.id === movie.id);
 
-  const watchlistDisabled = storeMovie ? true : false
+  const watchlistDisabled = storedMovie
+    ? true
+    : storedMovieWatched
+    ? true
+    : false;
+
+  const watchedDisabled = storedMovieWatched ? true : false;
 
 
-  return (
-    <div>
+    return (
       <div className="result-card">
         <div className="poster-wrapper">
           {movie.poster_path ? (
@@ -19,27 +31,36 @@ export const ResultsCard = ({ movie }) => {
               alt={`${movie.title} Poster`}
             />
           ) : (
-            <div className="filler-poster"></div>
+            <div className="filler-poster" />
           )}
         </div>
-
+  
         <div className="info">
           <div className="header">
             <h3 className="title">{movie.title}</h3>
-            <h4 className="release-data">
-              {movie.release_date ? movie.release_date.substring(0, 4) : "-"}
+            <h4 className="release-date">
+              <div format="YYYY">{movie.release_date}</div>
             </h4>
           </div>
-
+  
           <div className="controls">
-            <button 
-            disabled={watchlistDisabled}
-            onClick={() => addMovietoWatchlist(movie)} className="btn">
-              Add to WatchList
+            <button
+              className="btn"
+              disabled={watchlistDisabled}
+              onClick={() => addMovieToWatchlist(movie)}
+            >
+              Add to Watchlist
+            </button>
+  
+            <button
+              className="btn"
+              disabled={watchedDisabled}
+              onClick={() => addMovieToWatched(movie)}
+            >
+              Add to Watched
             </button>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
 };
